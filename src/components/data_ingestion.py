6bcaf0f9540c215188186ea9b55a19dc9d3c7b11
beraterkanelcelik <elcelikberaterkan@gmail.com>
2023-07-27 16:24:26 +0300

    model trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -8,6 +8,8 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
+from src.components.model_trainer import ModelTrainerConfig,ModelTrainer
+
 
 @dataclass
 class DataIngestionConfig:
@@ -50,4 +52,7 @@ if __name__=="__main__":
     train_data,test_data = obj.initiate_data_ingestion()
 
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_array, test_array, _ = data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer = ModelTrainer()
+    print("best model's r2_score: ",modeltrainer.initiate_model_trainer(train_array,test_array))
